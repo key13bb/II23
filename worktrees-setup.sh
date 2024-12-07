081b61137c125a2436e1161dce0ef8af807d9913
Francisco Bravo Bravo <key13bb@gmail.com>
2024-12-07 19:14:06 +0100

    Add --force to discard all changes before remounting all worktrees

--- worktrees-setup.sh
+++ worktrees-setup.sh
@@ -1,25 +1,25 @@
 #!/bin/sh
 
 # Umount all worktrees
-git worktree remove ./1.1/FELE
-git worktree remove ./1.1/IPRO
-git worktree remove ./1.1/MDIS
-git worktree remove ./1.1/OEMP
+git worktree remove ./1.1/FELE --force
+git worktree remove ./1.1/IPRO --force
+git worktree remove ./1.1/MDIS --force
+git worktree remove ./1.1/OEMP --force
 
-git worktree remove ./1.2/IISO
-git worktree remove ./1.2/PAV1
-git worktree remove ./1.2/TCOM
+git worktree remove ./1.2/IISO --force
+git worktree remove ./1.2/PAV1 --force
+git worktree remove ./1.2/TCOM --force
 
-git worktree remove ./2.1/ADAL
-git worktree remove ./2.1/BDAT
-git worktree remove ./2.1/ECOM
-git worktree remove ./2.1/MECO
+git worktree remove ./2.1/ADAL --force
+git worktree remove ./2.1/BDAT --force
+git worktree remove ./2.1/ECOM --force
+git worktree remove ./2.1/MECO --force
 
-git worktree remove ./2.2/FIAR
-git worktree remove ./2.2/PAV2
-git worktree remove ./2.2/RSER
-git worktree remove ./2.2/SOPE
-git worktree remove ./2.2/TALF
+git worktree remove ./2.2/FIAR --force
+git worktree remove ./2.2/PAV2 --force
+git worktree remove ./2.2/RSER --force
+git worktree remove ./2.2/SOPE --force
+git worktree remove ./2.2/TALF --force
 
 # Mount all worktrees
 git worktree add ./1.1/FELE 1.1/FELE
