bb2b0810ada6a453e82b388b5edf1dd44ae1495a
Francisco Bravo Bravo <key13bb@gmail.com>
2025-01-06 14:08:17 +0100

    Updated save and restore scripts

--- restore.sh
+++ restore.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Restore .idea config folders
-mv ../Backup/ADAL/.idea ./2.1/ADAL
-mv ../Backup/BDAT/.idea ./2.1/BDAT
-mv ../Backup/MECO/.idea ./2.1/MECO
+mv ../Backup/ADAL/.idea ./2.1/ADAL/.idea
+mv ../Backup/BDAT/.idea ./2.1/BDAT/.idea
+mv ../Backup/MECO/.idea ./2.1/MECO/.idea
 
 # Restore .vscode config folders
-mv ../Backup/ADAL/.vscode ./2.1/ADAL
-mv ../Backup/BDAT/.vscode ./2.1/BDAT
-mv ../Backup/MECO/.vscode ./2.1/MECO
-mv ../Backup/ECOM/.vscode ./2.1/ECOM
+mv ../Backup/ADAL/.vscode ./2.1/ADAL/.vscode
+mv ../Backup/BDAT/.vscode ./2.1/BDAT/.vscode
+mv ../Backup/MECO/.vscode ./2.1/MECO/.vscode
+mv ../Backup/ECOM/.vscode ./2.1/ECOM/.vscode
--- save.sh
+++ save.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # Save .idea config folders
-mv ./2.1/ADAL/.idea ../Backup/ADAL
-mv ./2.1/BDAT/.idea ../Backup/BDAT
-mv ./2.1/MECO/.idea ../Backup/MECO
+mv ./2.1/ADAL/.idea ../Backup/ADAL/.idea
+mv ./2.1/BDAT/.idea ../Backup/BDAT/.idea
+mv ./2.1/MECO/.idea ../Backup/MECO/.idea
 
 # Save .vscode config folders
-mv ./2.1/ADAL/.vscode ../Backup/ADAL
-mv ./2.1/BDAT/.vscode ../Backup/BDAT
-mv ./2.1/MECO/.vscode ../Backup/MECO
-mv ./2.1/ECOM/.vscode ../Backup/ECOM
+mv ./2.1/ADAL/.vscode ../Backup/ADAL/.vscode
+mv ./2.1/BDAT/.vscode ../Backup/BDAT/.vscode
+mv ./2.1/MECO/.vscode ../Backup/MECO/.vscode
+mv ./2.1/ECOM/.vscode ../Backup/ECOM/.vscode
