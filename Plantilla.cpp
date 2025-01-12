0b95249f37058b26d887b10bbec3168b9d311aef
Francisco Bravo Bravo <key13bb@gmail.com>
2025-01-12 21:58:34 +0100

    Added license to template

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -1,8 +1,17 @@
-#include <iostream>
+/*
+ * GNU GENERAL PUBLIC LICENSE
+ * Version 3, 29 June 2007
+ *
+ * Copyright (C) 2007 Free Software Foundation, Inc. <http://fsf.org/>
+ * Everyone is permitted to copy and distribute verbatim copies
+ * of this license document, but changing it is not allowed.
+ */
+
 #include <array>
+#include <iostream>
 
 using namespace std;
 
-int main() {
-
+int main()
+{
 }
